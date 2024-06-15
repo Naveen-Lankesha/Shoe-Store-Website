38221ab3b99e2121df2e0260f8f2cc8adb26297f
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-16 03:42:47 +0530

    Add UI for add items V1.0

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,53 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Order from "./pages/Order/Order";
 
+//theme imports
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// Create a custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FD7401",
+    },
+    secondary: {
+      main: "#FCA054",
+    },
+  },
+  typography: {
+    fontFamily: "Public Sans, sans-serif",
+    h1: {
+      fontWeight: 600,
+    },
+    h2: {
+      fontWeight: 600,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Navbar />
       <Box display={"flex"}>
-        <Sidebar />
-        <Routes>
-          <Route path={"/add"} element={<Add />} />
-          <Route path={"/List"} element={<List />} />
-          <Route path={"/orders"} element={<Order />} />
-        </Routes>
+        <Box flex={1}>
+          <Sidebar />
+        </Box>
+        <Box flex={4}>
+          <Routes>
+            <Route path={"/add"} element={<Add />} />
+            <Route path={"/List"} element={<List />} />
+            <Route path={"/orders"} element={<Order />} />
+          </Routes>
+        </Box>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 };
 
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import {
+  Box,
+  Stack,
+  TextField,
+  MenuItem,
+  FormControl,
+  Select,
+  InputLabel,
+  Button,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { assets } from "../../assets/assets";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [category, setCategory] = useState("");
+
+  const selectHandler = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const [image, setImage] = useState(false);
+
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    category: "",
+    price: "",
+  });
+
+  const onChangeHandler = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  // API call
+  const url = "http://localhost:4000";
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("price", Number(data.price));
+    formData.append("image", image);
+
+    const response = await axios.post(`${url}/api/shoe/add`, formData);
+    if (response.data.success) {
+      setData({ name: "", description: "", category: "", price: "" });
+      setImage(false);
+    } else {
+    }
+  };
+
+  return (
+    <Box sx={{ p: 8 }}>
+      <Stack spacing={3}>
+        <Box>
+          <Typography sx={{ color: "grey" }}>Upload Image</Typography>
+          <label htmlFor="image">
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              style={{ cursor: "pointer", maxWidth: 300 }}
+              alt="upload"
+            />
+          </label>
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            style={{ display: "none" }}
+          />
+        </Box>
+        <TextField
+          onChange={onChangeHandler}
+          value={data.name}
+          label="Product Name"
+          name="name"
+          required
+        />
+        <TextField
+          onChange={onChangeHandler}
+          value={data.description}
+          name="description"
+          label="Product Description"
+          required
+          multiline
+          rows={4}
+          defaultValue=""
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category *</InputLabel>
+          <Select
+            onChange={onChangeHandler}
+            name="category"
+            required
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Category">
+            <MenuItem value="Nike">Nike</MenuItem>
+            <MenuItem value="Adidas">Adidas</MenuItem>
+            <MenuItem value="Puma">Puma</MenuItem>
+            <MenuItem value="Reebok">Reebok</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          onChange={onChangeHandler}
+          name="price"
+          value={data.price}
+          label="Product Price (£)"
+          required
+        />
+
+        <form onSubmit={onSubmitHandler}>
+          <Button type="submit" variant="contained" sx={{ fontWeight: 600 }}>
+            Add
+          </Button>
+        </form>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Add;
