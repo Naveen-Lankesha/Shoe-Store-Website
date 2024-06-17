ec6131dff48aebcae92c020c884b6d6de8cded08
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-18 01:40:53 +0530

    Stripe V1.0 -back

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import shoeRouter from "./routes/shoeRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 import "dotenv/config";
 
 //-----------db pw: 26268
@@ -23,6 +24,7 @@ app.use("/api/shoe", shoeRouter);
 app.use("/images", express.static("uploads")); // Serving the uploaded images from the "uploads" folder by mounting the "/images" route to the "uploads" folder using express.static middleware
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
