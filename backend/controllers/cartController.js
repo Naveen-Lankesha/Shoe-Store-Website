9c0d957166ea76548785d2e5dbf10fe3b65609a0
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-17 19:25:14 +0530

    Remove from cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -19,7 +19,20 @@ const addToCart = async (req, res) => {
 };
 
 //remove items from user cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findOne({ _id: req.body.userId });
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.status(200).json({ message: "Item removed from cart" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 //fetch user cart data
 const getCart = async (req, res) => {};
