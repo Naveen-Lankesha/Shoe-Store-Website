16e379a6bc7a2b9e7ad854aa99a344f98f6bbf36
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-19 02:43:40 +0530

    Stop directing proceed to checkout without login or no items

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import { Stack, Box, TextField, Typography, Button } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
   const { getTotalCartAmount, token, shoe_list, cartItems, url } =
@@ -57,6 +58,16 @@ const PlaceOrder = () => {
       alert("Order Failed");
     }
   };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      navigate("/cart");
+    }
+  }, [token]);
 
   return (
     <form onSubmit={placeOrder}>
