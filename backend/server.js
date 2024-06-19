466baebc1de90b691c3f83cd0b3222bbba0b4a55
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-20 02:37:46 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import "dotenv/config";
 //-----------db pw: 26268
 //app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
