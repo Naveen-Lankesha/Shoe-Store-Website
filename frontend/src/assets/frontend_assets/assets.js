3ea138ab08ed1c81009bdbf7c2b49e99d5d3ad4f
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-06-20 02:00:08 +0530

    Removed unnecessary files

--- frontend/src/assets/frontend_assets/assets.js
+++ frontend/src/assets/frontend_assets/assets.js
@@ -7,10 +7,6 @@ import menu_1 from "./Adidas_logo.png";
 import menu_2 from "./Nike_logo.png";
 import menu_3 from "./Puma_logo.png";
 import menu_4 from "./Reebok_logo.png";
-// import menu_5 from "./menu_5.png";
-// import menu_6 from "./menu_6.png";
-// import menu_7 from "./menu_7.png";
-// import menu_8 from "./menu_8.png";
 
 //adidas
 import shoe_1 from "./shoe_1_adidas_campus 00s Youth trainers_70.jpg";
@@ -35,23 +31,6 @@ import shoe_13 from "./shoe_1_reebok_club c 85 trainers in white_75.jpg";
 import shoe_14 from "./shoe_1_reebok_club c bulc trainers in white & black_100.jpg";
 import shoe_15 from "./shoe_1_reebok_club c double trainers in white & burgundy_100.jpg";
 import shoe_16 from "./shoe_1_reebok_club c revenge trainers in white & green_75.jpg";
-
-// import food_17 from "./food_17.png";
-// import food_18 from "./food_18.png";
-// import food_19 from "./food_19.png";
-// import food_20 from "./food_20.png";
-// import food_21 from "./food_21.png";
-// import food_22 from "./food_22.png";
-// import food_23 from "./food_23.png";
-// import food_24 from "./food_24.png";
-// import food_25 from "./food_25.png";
-// import food_26 from "./food_26.png";
-// import food_27 from "./food_27.png";
-// import food_28 from "./food_28.png";
-// import food_29 from "./food_29.png";
-// import food_30 from "./food_30.png";
-// import food_31 from "./food_31.png";
-// import food_32 from "./food_32.png";
 
 import add_icon_white from "./add_icon_white.png";
 import add_icon_green from "./add_icon_green.png";
@@ -239,148 +218,4 @@ export const shoe_list = [
     description: "Club C Revenge trainers in white & green",
     category: "Reebok",
   },
-  // {
-  //   _id: "17",
-  //   name: "Cup Cake",
-  //   image: food_17,
-  //   price: 14,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Cake",
-  // },
-  // {
-  //   _id: "18",
-  //   name: "Vegan Cake",
-  //   image: food_18,
-  //   price: 12,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Cake",
-  // },
-  // {
-  //   _id: "19",
-  //   name: "Butterscotch Cake",
-  //   image: food_19,
-  //   price: 20,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Cake",
-  // },
-  // {
-  //   _id: "20",
-  //   name: "Sliced Cake",
-  //   image: food_20,
-  //   price: 15,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Cake",
-  // },
-  // {
-  //   _id: "21",
-  //   name: "Garlic Mushroom ",
-  //   image: food_21,
-  //   price: 14,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pure Veg",
-  // },
-  // {
-  //   _id: "22",
-  //   name: "Fried Cauliflower",
-  //   image: food_22,
-  //   price: 22,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pure Veg",
-  // },
-  // {
-  //   _id: "23",
-  //   name: "Mix Veg Pulao",
-  //   image: food_23,
-  //   price: 10,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pure Veg",
-  // },
-  // {
-  //   _id: "24",
-  //   name: "Rice Zucchini",
-  //   image: food_24,
-  //   price: 12,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pure Veg",
-  // },
-  // {
-  //   _id: "25",
-  //   name: "Cheese Pasta",
-  //   image: food_25,
-  //   price: 12,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pasta",
-  // },
-  // {
-  //   _id: "26",
-  //   name: "Tomato Pasta",
-  //   image: food_26,
-  //   price: 18,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pasta",
-  // },
-  // {
-  //   _id: "27",
-  //   name: "Creamy Pasta",
-  //   image: food_27,
-  //   price: 16,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pasta",
-  // },
-  // {
-  //   _id: "28",
-  //   name: "Chicken Pasta",
-  //   image: food_28,
-  //   price: 24,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Pasta",
-  // },
-  // {
-  //   _id: "29",
-  //   name: "Buttter Noodles",
-  //   image: food_29,
-  //   price: 14,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Noodles",
-  // },
-  // {
-  //   _id: "30",
-  //   name: "Veg Noodles",
-  //   image: food_30,
-  //   price: 12,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Noodles",
-  // },
-  // {
-  //   _id: "31",
-  //   name: "Somen Noodles",
-  //   image: food_31,
-  //   price: 20,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Noodles",
-  // },
-  // {
-  //   _id: "32",
-  //   name: "Cooked Noodles",
-  //   image: food_32,
-  //   price: 15,
-  //   description:
-  //     "Food provides essential nutrients for overall health and well-being",
-  //   category: "Noodles",
-  // },
 ];
